a1733ba25618827a2f0889e85bc2cf5d4d7f7e1b
bhanu <bhanu@bhanu.(none)>
2013-12-30 22:31:02 +0530

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,22 @@
+class HomeController < ApplicationController
+  def aboutus
+  end
+
+  def contactus
+  end
+
+  def demo
+  end
+
+  def job
+  end
+
+  def portfolio
+  end
+
+  def services
+  end
+
+  def team
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Datracker::Application.config.secret_token = 'd7e9d4f5d95f469783f5f2e62a52fce6c385eb51eddd265451981c9dcb43747132256b0dd729deca0ed9eefd032527178b29bcddb9cb9c8e4d47d8633b811314'
